f3d4ced56ee45b75129276d3e491ad247f39ddc5
desilauraa <desilauraa@gmail.com>
2023-11-22 08:09:18 +0700

    first commit

---
+++ database/migrations/2023_11_17_055228_create_kategoris_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKategorisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kategoris', function (Blueprint $table) {
+            $table->id();
+            $table->string('kategori_kelas')->nullable();
+            // $table->string('nama_guru');
+            // $table->string('menu_url')->nullable();
+            // $table->string('status')->nullable();
+            // $table->longText('content')->nullable();
+            $table->string('type')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kategoris');
+    }
+}
